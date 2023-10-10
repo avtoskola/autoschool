235160d9a290aac988710b30157cfecd58a53b66
KonstantinKliukhin <kliukhinkonstantin@gmail.com>
2023-10-10 14:16:31 +0300

    added hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 // eslint-disable-next-line camelcase
 import { Noto_Sans } from 'next/font/google';
-import Script from 'next/script';
 
 import RootLayout from '@/components/layout';
 
@@ -14,21 +13,10 @@ const font = Noto_Sans({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
-      <Script id="google-analytics">
-        {`
-           window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-
-             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-         `}
-      </Script>
       <div className={font.className}>
         <RootLayout>
           <Component {...pageProps} />
         </RootLayout>
-
       </div>
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,33 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
         <link rel="icon" href="/images/favicon.ico" />
+        <Script id="hotjar">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3687975,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+        <Script id="google-analytics">
+          {`
+           window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+
+             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+         `}
+        </Script>
       </Head>
       <body>
         <Main />
