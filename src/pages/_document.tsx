1772f82e3d3ae85f8c4919ea86f1a130ffd913ae
avtoskola <avtoskola88@gmail.com>
2023-11-03 11:28:25 +0200

    added og description, moved favicon to be in root folder, added self-referral canonical url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,13 @@ import translations from '../../public/locales/ua.json';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-        <link rel="icon" href="/images/favicon.ico" />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VARIFICATION_ID} />
         <meta property="og:title" content={translations['НАЗВА СТОРІНКИ']}/>
+        <meta property="og:description" content={translations['КОРОТКИЙ ОПИС СТОРІНКИ']}/>
         <meta property="og:image" content="/images/auto-1.webp"/>
+        <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_HOME_URL}/>
       </Head>
       <body>
         <Main />
