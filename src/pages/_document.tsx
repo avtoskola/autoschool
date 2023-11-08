786fdc2fdde0c01f02dc9facc22dbd87801c6931
avtoskola <avtoskola88@gmail.com>
2023-11-08 14:52:05 +0200

    changed favicon preset, edited keywords

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VARIFICATION_ID} />
         <meta property="og:title" content={translations['НАЗВА СТОРІНКИ']}/>
         <meta property="og:description" content={translations['КОРОТКИЙ ОПИС СТОРІНКИ']}/>
-        <meta property="og:image" content="/images/auto-1.webp"/>
+        <meta property="og:image" content="/android-chrome-512x512.png"/>
         <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_HOME_URL}/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
       <body>
         <Main />
